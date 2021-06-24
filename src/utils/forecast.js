10df73b813489edccdc664cc36abb20321feb357
Eslam Hamdy <eslam_hamdy_95@hotmail.com>
2021-06-24 14:42:56 +0200

    adding more info about forecast and working on UI

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude , longitude , callback) => {
         }  else if (body.error) {
             callback('Unable to find location!' , undefined)
         } else {
-            callback(undefined , body.current.weather_descriptions[0] + ' It is currently ' +  body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike)
+            callback(undefined , body.current.weather_descriptions[0] + ', It is currently ' +  body.current.temperature + '\u00B0 degree. It feels like ' + body.current.feelslike + '\u00B0' + ', Wind speed is ' + body.current.wind_speed + ' km/h, Visibility is ' + body.current.visibility + ' km, Humidity is ' + body.current.humidity + '%, Pressure is ' + body.current.pressure + ' mb, Chance of rain is ' + body.current.precip + '%.')
         }
     })
 }
